a235600cad61ddfd0d6638c455bcf3f7889a6030
Tejas Joshi <tejasjoshi4598@gmail.com>
2026-04-13 20:03:41 +0530

    Updated KMK firmware

--- Firmware/KMK/code.py
+++ Firmware/KMK/code.py
@@ -8,15 +8,14 @@ from kmk.scanners import DiodeOrientation
 
 keyboard = KMKKeyboard()
 
-keyboard.col_pins = (board.D10, board.D9, board.D8)
-keyboard.row_pins = (board.D0, board.D1, board.D2, board.D3)
+keyboard.col_pins = (board.GP20, board.GP19, board.GP18)
+keyboard.row_pins = (board.GP6, board.GP7, board.GP8, board.GP9)
 keyboard.diode_orientation = DiodeOrientation.COL2ROW
 
 keyboard.keymap = [
-    [KC.A, KC.B, KC.C, KC.D],
-    [KC.E, KC.F, KC.G, KC.H],
-    [KC.I, KC.J, KC.K, KC.L]
+    [KC.A, KC.B, KC.C, KC.D, KC.E, KC.F, KC.G, KC.H, KC.I, KC.J, KC.K, KC.L]
 ]
 
 if __name__ == '__main__':
     keyboard.go()
+    
